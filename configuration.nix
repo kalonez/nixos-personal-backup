107a5de5f2d22311b348b6a7c24415a3655c9dda
kalonez <imkalonez@gmail.com>
2025-11-29 20:31:01 +0400

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -57,10 +57,6 @@
   };
 };
 
-  
-  services.blueman.enable = true;
-
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
