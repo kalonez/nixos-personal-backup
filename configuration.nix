54eccb4585425703512d9a3051e5e88140170081
kalonez <imkalonez@gmail.com>
2025-12-03 14:33:33 +0400

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -143,6 +143,7 @@ in
   # Install firefox.
   programs.firefox.enable = true;
 
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -225,6 +226,6 @@ in
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 
 }
